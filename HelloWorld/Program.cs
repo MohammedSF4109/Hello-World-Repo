dfbe286c196371c725508b59572299a42d3b34ba
MohammedSF4109 <mohammed.sf4109@gmail.com>
2023-03-06 15:36:35 +0530

    Code added

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,14 @@
 ﻿using System;
-Console.WriteLine("Hello, World!");
+
+namespace HelloWorld
+{
+    class Program{
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter Your Name: ");
+            string name = Console.ReadLine();
+            System.Console.WriteLine($"Hello {name}");
+            Console.ReadLine();
+        }
+    }
+}
